5718a584497bf78289c12f33e59ecf86d1db4abd
David Dufresne <dufresne.david.2@gmail.com>
2018-06-15 14:55:17 -0400

    Codacy fix

--- hr_expense_tax_adjustment/models/hr_expense_tax.py
+++ hr_expense_tax_adjustment/models/hr_expense_tax.py
@@ -1,7 +1,7 @@
 # © 2018 Numigi
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 from odoo.addons import decimal_precision as dp
 
 
