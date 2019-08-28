e8729b07f3ec91add3d52ae96cd750a8fecddfb9
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-08-28 15:21:24 -0400

    Add module old_accounts (#40)

---
+++ old_accounts/account.py
@@ -0,0 +1,14 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models, fields
+
+
+class Account(models.Model):
+
+    _inherit = 'account.account'
+
+    old_accounts = fields.Text(
+        help="This field allows to list related account numbers from previous systems "
+        "and notes related to these."
+    )
