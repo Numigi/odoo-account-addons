9186d4d85a6158c91ef7c63381ab90d6a9497ad2
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-10-23 13:20:14 -0400

    Codacy

--- account_payment_widget_link/tests/test_account_move_line.py
+++ account_payment_widget_link/tests/test_account_move_line.py
@@ -1,7 +1,6 @@
 # © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/LGPL.html).
 
-from datetime import datetime
 from odoo.tests.common import SavepointCase
 
 
