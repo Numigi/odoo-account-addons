5a660dc2768857d32f6753ac7259729bd762cd2b
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-11-27 13:21:18 -0500

    Fix module invoice_currency_validation
    Improve the module implementation.
    Add extra unit tests.
    Improve the README.

--- invoice_currency_validation/models/account_invoice.py
+++ invoice_currency_validation/models/account_invoice.py
@@ -4,6 +4,32 @@
 
 from odoo import _, api, models
 from odoo.exceptions import UserError
+from typing import Optional
+
+
+def _find_matching_invoice_journal(
+    company: 'res.company',
+    invoice_type: str,
+    invoice_currency: 'res.currency',
+) -> Optional['account.journal']:
+    """Find a matching journal given a type of invoice and a currency.
+
+    :param company: the company of the invoice
+    :param invoice_type: the type of invoice
+    :param invoice_currency: the invoice currency
+    :return: the first matching journal if any found
+    """
+    journal_type = (
+        'sale' if invoice_type in ('out_invoice', 'out_refund') else 'purchase'
+    )
+    candidate_journals = company.env['account.journal'].search([
+        ('type', '=', journal_type),
+        ('company_id', '=', company.id),
+    ], order='sequence')
+    return next((
+        j for j in candidate_journals
+        if (j.currency_id or company.currency_id) == invoice_currency
+    ), None)
 
 
 class AccountInvoice(models.Model):
@@ -11,94 +37,54 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     @api.onchange('currency_id')
-    def _onchange_currency_id(self):
-        journal_currency = (
-            self.journal_id.currency_id or
-            self.journal_id.company_id.currency_id)
+    def _onchange_currency_set_journal(self):
+        if self.currency_id:
+            self.journal_id = _find_matching_invoice_journal(
+                self.company_id, self.type, self.currency_id
+            )
+        else:
+            self.journal_id = None
 
-        if journal_currency != self.currency_id:
-            journal_type = 'sale' if self.type == 'out_invoice' else 'purchase'
-            journal_id = self.env['account.journal'].search([
-                ('type', '=', journal_type),
-                '|',
-                ('currency_id', '=', self.currency_id.id),
-                ('currency_id', '=', False)
-            ], order='sequence', limit=1)
+    def _check_invoice_currency_versus_journal_currency(self):
+        company_currency = self.company_id.currency_id
+        invoice_currency = self.currency_id or company_currency
+        journal_currency = self.journal_id.currency_id or company_currency
 
-            if journal_id:
-                self.journal_id = journal_id
+        if journal_currency != invoice_currency:
+            raise UserError(_(
+                "The invoice could not be validated. "
+                "The invoice is in currency {invoice_currency} "
+                "and the selected journal ({journal}) is in "
+                "currency {journal_currency}."
+            ).format(
+                invoice=self.display_name,
+                invoice_currency=invoice_currency.display_name,
+                journal=self.journal_id.display_name,
+                journal_currency=journal_currency.name,
+            ))
+
+    def _check_invoice_currency_versus_account_currency(self):
+        company_currency = self.company_id.currency_id
+        invoice_currency = self.currency_id or company_currency
+        account_currency = self.account_id.currency_id or company_currency
+
+        if account_currency != invoice_currency:
+            raise UserError(_(
+                "The invoice could not be validated. "
+                "The invoice is in currency {invoice_currency} "
+                "and the selected account ({account}) is in "
+                "currency {account_currency}."
+            ).format(
+                invoice=self.display_name,
+                invoice_currency=invoice_currency.display_name,
+                account=self.account_id.display_name,
+                account_currency=account_currency.name,
+            ))
 
     @api.multi
     def action_invoice_open(self):
         for invoice in self:
-            invoice._check_currency()
+            invoice._check_invoice_currency_versus_journal_currency()
+            invoice._check_invoice_currency_versus_account_currency()
 
-        return super(AccountInvoice, self).action_invoice_open()
-
-    def _check_currency(self):
-        invoice_currency = self.currency_id
-        journal_currency = self.journal_id.currency_id
-        account_currency = self.account_id.currency_id
-        company_currency = self.company_id.currency_id
-
-        if not journal_currency and not account_currency:
-            return
-
-        if (
-            journal_currency and not account_currency and
-            journal_currency != company_currency
-        ):
-            raise UserError(_(
-                "The invoice %(invoice_name)s could not be validated. "
-                "The selected journal (%(journal_name)s) has a currency "
-                "(%(journal_currency)s) and the selected account "
-                "(%(account_code)s) has no currency.") % {
-                    'invoice_name': self.display_name,
-                    'journal_name': self.journal_id.name,
-                    'journal_currency': journal_currency.name,
-                    'account_code': self.account_id.code,
-            })
-
-        if (
-            not journal_currency and account_currency and
-            account_currency != company_currency
-        ):
-            raise UserError(_(
-                "The invoice %(invoice_name)s could not be validated. "
-                "The selected journal (%(journal_name)s) has no currency "
-                "and the selected account (%(account_code)s) has a "
-                "currency (%(account_currency)s).") % {
-                    'invoice_name': self.display_name,
-                    'journal_name': self.journal_id.name,
-                    'account_code': self.account_id.code,
-                    'account_currency': account_currency.name,
-            })
-
-        if (
-            journal_currency and account_currency and
-            journal_currency != account_currency
-        ):
-            raise UserError(_(
-                "The invoice %(invoice_name)s could not be validated. "
-                "The selected journal (%(journal_name)s) has a currency "
-                "(%(journal_currency)s) and the selected account "
-                "(%(account_code)s) has a currency "
-                "(%(account_currency)s).") % {
-                    'invoice_name': self.display_name,
-                    'journal_name': self.journal_id.name,
-                    'journal_currency': journal_currency.name,
-                    'account_code': self.account_id.code,
-                    'account_currency': account_currency.name,
-            })
-
-        if (journal_currency or company_currency) != invoice_currency:
-            raise UserError(_(
-                "The invoice %(invoice_name)s could not be validated. "
-                "The selected journal (%(journal_name)s) has a currency "
-                "(%(journal_currency)s) and the invoice has a currency "
-                "(%(invoice_currency)s).") % {
-                    'invoice_name': self.display_name,
-                    'journal_name': self.journal_id.name,
-                    'journal_currency': journal_currency.name,
-                    'invoice_currency': invoice_currency.name,
-            })
+        return super().action_invoice_open()
--- invoice_currency_validation/tests/test_account_invoice.py
+++ invoice_currency_validation/tests/test_account_invoice.py
@@ -2,18 +2,28 @@
 # © 2018 Numigi
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+from ddt import ddt, data, unpack
 from odoo.tests.common import SavepointCase
 from odoo.exceptions import UserError
 
 
+@ddt
 class TestAccountInvoice(SavepointCase):
 
     @classmethod
     def setUpClass(cls):
-        super(TestAccountInvoice, cls).setUpClass()
-
+        super().setUpClass()
+        cls.company_currency = cls.env.ref('base.USD')
         cls.currency_cad = cls.env.ref('base.CAD')
         cls.currency_eur = cls.env.ref('base.EUR')
+
+        cls.company = cls.env['res.company'].create({
+            'name': 'New Company',
+            'currency_id': cls.company_currency.id,
+        })
+
+        cls.env.user.company_ids |= cls.company
+        cls.env.user.company_id = cls.company
 
         cls.product = cls.env['product.product'].create({
             'name': 'Product',
@@ -23,6 +33,7 @@ class TestAccountInvoice(SavepointCase):
             'name': 'Payable Account',
             'code': '1706',
             'reconcile': True,
+            'company_id': cls.company.id,
             'user_type_id': cls.env.ref(
                 'account.data_account_type_payable').id,
         })
@@ -30,14 +41,31 @@ class TestAccountInvoice(SavepointCase):
         cls.account_2 = cls.env['account.account'].create({
             'name': 'Expenses Account',
             'code': '1708',
+            'company_id': cls.company.id,
             'user_type_id': cls.env.ref(
                 'account.data_account_type_expenses').id,
         })
 
         cls.journal = cls.env['account.journal'].create({
             'name': 'Journal',
-            'type': 'bank',
-            'code': 'JR1717',
+            'type': 'purchase',
+            'code': 'PJUSD',
+            'company_id': cls.company.id,
+        })
+
+        cls.journal_cad = cls.env['account.journal'].create({
+            'name': 'Journal',
+            'type': 'purchase',
+            'code': 'PJCAD',
+            'currency_id': cls.currency_cad.id,
+            'company_id': cls.company.id,
+        })
+
+        cls.sale_journal = cls.env['account.journal'].create({
+            'name': 'Journal',
+            'type': 'sale',
+            'code': 'SJCAD',
+            'company_id': cls.company.id,
         })
 
         cls.supplier = cls.env['res.partner'].create({
@@ -55,33 +83,78 @@ class TestAccountInvoice(SavepointCase):
             'partner_id': cls.supplier.id,
             'journal_id': cls.journal.id,
             'account_id': cls.account_1.id,
-            'currency_id': cls.currency_eur.id,
+            'currency_id': cls.company_currency.id,
+            'company_id': cls.company.id,
             'invoice_line_ids': [(4, cls.line.id)],
             'type': 'in_invoice',
         })
 
-    def test_01_journal_without_currency_and_account_without_currency(self):
+    def _validate_invoice(self):
         self.invoice.action_invoice_open()
+
+    def test_journal_without_currency_and_account_without_currency(self):
+        self._validate_invoice()
         self.assertEqual(self.invoice.state, 'open')
 
-    def test_02_journal_with_currency_and_account_without_currency(self):
-        self.journal.write({'currency_id': self.currency_cad.id})
+    def test_invoice_with_currency_and_journal_without_currency(self):
+        self.invoice.currency_id = self.currency_cad
         with self.assertRaises(UserError):
-            self.invoice.action_invoice_open()
+            self._validate_invoice()
 
-    def test_03_journal_without_currency_and_account_with_currency(self):
-        self.account_1.write({'currency_id': self.currency_cad.id})
+    def test_journal_with_currency_and_account_without_currency(self):
+        self.journal.currency_id = self.currency_cad
         with self.assertRaises(UserError):
-            self.invoice.action_invoice_open()
+            self._validate_invoice()
 
-    def test_04_journal_and_account_with_different_currencies(self):
-        self.journal.write({'currency_id': self.currency_cad.id})
-        self.account_1.write({'currency_id': self.currency_eur.id})
+    def test_journal_without_currency_and_account_with_currency(self):
+        self.account_1.currency_id = self.currency_cad
         with self.assertRaises(UserError):
-            self.invoice.action_invoice_open()
+            self._validate_invoice()
 
-    def test_05_journal_and_invoice_with_different_currencies(self):
-        self.journal.write({'currency_id': self.currency_cad.id})
-        self.account_1.write({'currency_id': self.currency_cad.id})
+    def test_journal_and_account_with_different_currencies(self):
+        self.journal.currency_id = self.currency_cad
+        self.account_1.currency_id = self.currency_eur
         with self.assertRaises(UserError):
-            self.invoice.action_invoice_open()
+            self._validate_invoice()
+
+    def test_journal_and_invoice_with_different_currencies(self):
+        self.journal.currency_id = self.currency_cad
+        self.account_1.currency_id = self.currency_cad
+        with self.assertRaises(UserError):
+            self._validate_invoice()
+
+    def _trigger_onchange(self):
+        self.invoice._onchange_currency_set_journal()
+
+    def test_onchange_currency_with_foreign_currency(self):
+        self.invoice.currency_id = self.currency_cad
+        self._trigger_onchange()
+        assert self.invoice.journal_id == self.journal_cad
+
+    def test_onchange_currency_with_company_currency(self):
+        self.invoice.currency_id = self.company_currency
+        self.invoice.journal_id = self.journal_cad
+        self._trigger_onchange()
+        assert self.invoice.journal_id == self.journal
+
+    def test_onchange_currency__journal_with_lower_sequence_selected(self):
+        self.journal.sequence = 1
+        other_journal = self.journal.copy({
+            'name': 'Other Journal',
+            'sequence': 0,
+            'code': 'PJUSD2',
+        })
+        self._trigger_onchange()
+        assert self.invoice.journal_id == other_journal
+
+    @data(
+        ('in_invoice', 'purchase'),
+        ('in_refund', 'purchase'),
+        ('out_invoice', 'sale'),
+        ('out_refund', 'sale'),
+    )
+    @unpack
+    def test_onchange_currency__correct_journal_type_selected(self, invoice_type, journal_type):
+        self.invoice.type = invoice_type
+        self._trigger_onchange()
+        assert self.invoice.journal_id.type == journal_type
