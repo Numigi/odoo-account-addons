e09f94428007f1be0c0081e76aaad3171ad9a438
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-04-16 15:15:12 -0400

    Update account_account_type.py

--- account_type_archive/models/account_account_type.py
+++ account_type_archive/models/account_account_type.py
@@ -1,7 +1,7 @@
 # © 2020 - today Numigi
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class AccountType(models.Model):
