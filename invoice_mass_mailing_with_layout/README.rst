e7122fb71099a3ce6ff255d55b7526f1598697c2
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-01-04 13:32:28 -0500

    TA#30342 add module invoice_mass_mailing_with_layout (#55)
    * TA#30342 add module invoice_mass_mailing_with_layout
    
    * Remove unrequired unit test

---
+++ invoice_mass_mailing_with_layout/README.rst
@@ -0,0 +1,32 @@
+Invoice Mass Mailing With Layout
+================================
+
+.. contents:: Table of Contents
+
+Context
+-------
+In vanilla Odoo, when sending an invoice from the ``Send && Print`` button,
+the email is sent using a custom layout.
+
+.. image:: static/description/invoice_send_print_button.png
+
+.. image:: static/description/email_custom_layout.png
+
+However, when sending the invoice using the mass mailing feature, the layout is not used.
+
+.. image:: static/description/invoice_mass_mail_button.png
+
+.. image:: static/description/email_without_custom_layout.png
+
+Usage
+-----
+After installing this module, when sending the invoice using the mass mailing feature,
+the email is sent using the custom layout.
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+
+More information
+----------------
+* Meet us at https://bit.ly/numigi-com
---
+++ invoice_mass_mailing_with_layout/tests/test_account_invoice_send.py
@@ -0,0 +1,50 @@
+# © 2019 Numigi
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import pytest
+from datetime import datetime, timedelta
+from odoo.tests.common import SavepointCase
+from odoo.exceptions import ValidationError
+
+
+class TestAccountInvoice(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.invoices = cls.env["account.invoice"].search([("type", "=", "out_invoice")])
+        cls.template = cls.env.ref("account.email_template_edi_invoice")
+        cls.template.auto_delete = False
+
+    def test_send_multiple_invoices(self):
+        wizard = self._create_wizard(self.invoices)
+        wizard.send_and_print_action()
+
+        email_1 = self._find_last_email(self.invoices[0])
+        self._check_email_has_layout(email_1)
+
+        email_2 = self._find_last_email(self.invoices[1])
+        self._check_email_has_layout(email_2)
+
+    def _check_email_has_layout(self, email):
+        assert email
+        assert "<table" in email.body_html
+
+    def _create_wizard(self, invoices):
+        wizard_obj = self.env["account.invoice.send"].with_context(
+            active_ids=invoices.ids,
+            active_model="account.invoice",
+        )
+        defaults = wizard_obj.default_get(list(wizard_obj._fields))
+        wizard = wizard_obj.create(defaults)
+        wizard.template_id = self.template
+        wizard.invoice_ids = invoices
+        wizard.is_email = True
+        wizard._compute_composition_mode()
+        return wizard
+
+    def _find_last_email(self, invoice):
+        return self.env["mail.mail"].search(
+            [("model", "=", "account.invoice"), ("res_id", "=", invoice.id),],
+            order="id desc",
+            limit=1,
+        )
