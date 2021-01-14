5672645acb0bfafd07691eb507182f8fa6b0bc8c
cuong.nmtm <cuong.nmtm@komit-consulting.com>
2021-01-14 22:45:16 +0700

    Remove .pyc and minor adjustments

--- account_move_unique_reversal/__manifest__.py
+++ account_move_unique_reversal/__manifest__.py
@@ -1,9 +1,10 @@
-# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Account Move Unique Reversal",
-    "summary": "Block the users to reverse the Journal Entries while they have already reversed or they are the reversal of the other Entries",
+    "summary": "Block the users to reverse the Journal Entries while they have already "
+    "reversed or they are the reversal of the other Entries",
     "version": "12.0.1.0.0",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",
