4dc65d06c8599367d9e19c8c7f140e0b3f87a7c3
🌴Cuong, Nguyen Minh Tran Manh <cuong.nmtm@komit-consulting.com>
2021-01-14 11:58:40 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- account_move_unique_reversal/__manifest__.py
+++ account_move_unique_reversal/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Move Unique Reversal",
-    "summary": "",
+    "summary": "Block the users to reverse the Journal Entries while they have already reversed or they are the reversal of the other Entries",
     "version": "12.0.1.0.0",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",
