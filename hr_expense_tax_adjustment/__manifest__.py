fb61b79c91cee91ac963d381e169f6b9df29496a
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-02-17 08:28:18 -0500

    Bump module version

--- hr_expense_tax_adjustment/__manifest__.py
+++ hr_expense_tax_adjustment/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Expense Tax Adjustment',
-    'version': '1.0.0',
+    'version': '1.1.0',
     'author': 'Numigi',
     'maintainer': 'Numigi',
     'website': 'https://www.numigi.com',
