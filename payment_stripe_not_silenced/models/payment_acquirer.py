c3cf8428a7d2a2b967bdfc4efec75bcc4aaa6b9f
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-03-17 15:53:08 -0400

    TA#32326 log all responses from stripe

--- payment_stripe_not_silenced/models/payment_acquirer.py
+++ payment_stripe_not_silenced/models/payment_acquirer.py
@@ -1,7 +1,11 @@
 # © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+import logging
 from odoo import models
+
+
+_logger = logging.getLogger(__name__)
 
 
 class PaymentAcquirer(models.Model):
@@ -10,4 +14,6 @@ class PaymentAcquirer(models.Model):
 
     def _stripe_request(self, url, data=False, method="POST"):
         self = self.with_context(stripe_manual_payment=True)
-        return super(PaymentAcquirer, self)._stripe_request(url, data, method)
+        res = super(PaymentAcquirer, self)._stripe_request(url, data, method)
+        _logger.info("Response from Stripe: {}".format(res))
+        return res
