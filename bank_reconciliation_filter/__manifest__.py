82c53325f3ba9a53eaac5ed032466acb72731e80
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-12-01 15:59:34 -0500

    TA#41006 finalize module and add documentation (#90)
    * TA#41006 finalize module and add documentation
    
    * Update translations

---
+++ bank_reconciliation_filter/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Bank Reconciliation Filter",
+    "summary": "Filter the list of journal entries proposed by the reconciliation widget",
+    "version": "1.0.0",
+    "website": "https://bit.ly/numigi-com",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/account_journal.xml",
+    ],
+    "installable": True,
+}
