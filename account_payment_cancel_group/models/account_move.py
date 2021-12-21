f25c8aba6bdd42bb270fa96fc1084e2361c1877e
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-12-21 15:41:42 -0500

    Add restriction to draft button and improve documentation

--- account_payment_cancel_group/models/account_move.py
+++ account_payment_cancel_group/models/account_move.py
@@ -10,10 +10,16 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     def button_draft(self):
-        if self._contains_payments() and not self._user_can_cancel_payments():
-            raise AccessError(_("You are not authorized to cancel payments."))
-
+        self._check_payment_cancel_authorization()
         return super().button_draft()
+
+    def button_cancel(self):
+        self._check_payment_cancel_authorization()
+        return super().button_cancel()
+
+    def _check_payment_cancel_authorization(self):
+        if self._contains_payments() and not self._user_can_cancel_payments():
+            raise AccessError(_("You are not authorized to reset to draft or cancel payments."))
 
     def _user_can_cancel_payments(self):
         return self.env.user.has_group(
