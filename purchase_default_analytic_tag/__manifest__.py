776aa9f9ebf302619e5fce3534caf4c657b7e3cc
Hieu.VMB <hieu.vmb@komit-consulting.com>
2022-02-11 09:36:26 +0700

    T#009078 - PO18000212 - #41180 - Tags analytiques par défaut sur Achats

---
+++ purchase_default_analytic_tag/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Purchase Default Analytic Tag",
+    "version": "1.1.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "LGPL-3",
+    "category": "Account",
+    "depends": ["analytic", "account", "purchase"],
+    "data": [
+        # views
+        "views/product_template.xml",
+    ],
+    "installable": True,
+}
