8b09f67807dd751bdfd425a6058f95628f0d1306
Quentin Lavallée-Bourdeau <31708522+qtiplb@users.noreply.github.com>
2022-03-10 13:32:40 -0500

    TA#41563 add Account Invoice Advance Report (#107)
    * T#009079-PO18000212-#41198- Dimension Projet sur ligne de PO et facture fournisseur
    
    * T#009081 - PO18000212 - #41563 - Vue liste des lignes de factures
    
    Co-authored-by: Hieu.VMB <hieu.vmb@komit-consulting.com>

---
+++ account_invoice_advance_report/models/account_invoice_line.py
@@ -0,0 +1,9 @@
+# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from odoo import fields, models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
+
+    invoice_date = fields.Date(related="invoice_id.date_invoice", store=True)
