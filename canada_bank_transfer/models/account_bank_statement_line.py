84699ed8b2207ef507112a2c2504e103faf498e7
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2022-04-12 22:09:49 -0400

    TA#42980 auto update eft payment status
    When an EFT is done through a transit account,
    on bank reconciliation, the payments are left
    at the status sent.
    
    The status should be reconciled when the batch payment
    is reconciled with the bank statement.

---
+++ canada_bank_transfer/models/account_bank_statement_line.py
@@ -0,0 +1,33 @@
+# © 2019 Numigi
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class AccountBankStatementLine(models.Model):
+
+    _inherit = "account.bank.statement.line"
+
+    def process_reconciliation(self, *args, **kwargs):
+        res = super().process_reconciliation(*args, **kwargs)
+
+        if self.journal_id.use_transit_account:
+            payments = self._get_transit_eft_payments()
+            payments.write({"state": "reconciled"})
+
+        return res
+
+    def button_cancel_reconciliation(self):
+        if self.journal_id.use_transit_account:
+            payments = self._get_transit_eft_payments()
+            payments.write({"state": "sent"})
+
+        super().button_cancel_reconciliation()
+
+    def _get_transit_eft_payments(self):
+        payments = self.mapped(
+            "journal_entry_ids.move_id.line_ids"
+            ".matched_credit_ids.credit_move_id.payment_id"
+        )
+        return payments.filtered("is_eft_payment")
