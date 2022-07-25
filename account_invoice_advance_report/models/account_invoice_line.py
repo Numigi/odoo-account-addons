1489c838aa47ccea80c1fbc8a3561e7eedf355bb
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2022-07-25 17:02:35 +0100

    TA#43629 Update ref to account_invoice_line (#123)

--- account_invoice_advance_report/models/account_invoice_line.py
+++ account_invoice_advance_report/models/account_invoice_line.py
@@ -1,10 +1,20 @@
 # © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
 
     invoice_date = fields.Date(related="invoice_id.date_invoice", store=True)
-    invoice_reference = fields.Char(related="invoice_id.reference", store=True)
+    # old field to remove
+    invoice_reference = fields.Char()
+    invoice_ref = fields.Char(compute="_compute_invoice_reference",
+                              string="Invoice Reference",
+                              store=True)
+
+    @api.depends("invoice_id", "invoice_id.reference")
+    def _compute_invoice_reference(self):
+        for rec in self:
+            if rec.invoice_id:
+                rec.invoice_ref = rec.invoice_id.reference
