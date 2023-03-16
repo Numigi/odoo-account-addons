f38191218472cd511575531fb3cc3d9eeec765a4
houdaBENTALEB <houda.bentaleb@gmail.com>
2023-03-16 22:42:04 +0100

    TA#50376 [FIX]account_move_unique_reversal: up module version and Readme file

--- account_move_unique_reversal/__manifest__.py
+++ account_move_unique_reversal/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Account Move Unique Reversal",
     "summary": "Blocks the users from reversing the Journal Entries if they have "
     "already been reversed or they are the reversal of the other Entries",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.1",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",
     "maintainer": "Numigi",
