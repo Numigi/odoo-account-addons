146b9e118abe3ed69da3801a99ff7080284c3897
houdaBENTALEB <houda.bentaleb@gmail.com>
2023-03-16 22:37:48 +0100

    TA#50376 [FIX]account_move_unique_reversal: Exclude the application of the error message blocking accounting entries for sale or purchase the journal

--- account_move_unique_reversal/tests/test_account_move_unique_reversal.py
+++ account_move_unique_reversal/tests/test_account_move_unique_reversal.py
@@ -13,12 +13,23 @@ class TestAccountMoveUniqueReversal(common.SavepointCase):
         cls.journal = cls.env["account.journal"].create(
             {"name": "Test", "code": "TEST", "type": "general"}
         )
+        cls.journal_sale = cls.env['account.journal'].create({
+            'name': 'Test Sales Journal',
+            'code': 'tSAL',
+            'type': 'sale',
+        })
+        cls.journal_purchase = cls.env['account.journal'].create({
+            'name': 'Test Purchase Journal',
+            'code': 'tPO',
+            'type': 'purchase',
+        })
         cls.today = fields.date.today()
         cls.account_1 = cls.env["account.account"].create(
             {
                 "name": "Account 1",
                 "code": "501001",
-                "user_type_id": cls.env.ref("account.data_account_type_expenses").id,
+                "user_type_id": cls.env.ref(
+                    "account.data_account_type_expenses").id,
             }
         )
         cls.account_2 = cls.env["account.account"].create(
@@ -30,27 +41,44 @@ class TestAccountMoveUniqueReversal(common.SavepointCase):
                 ).id,
             }
         )
-        cls.move = cls.env["account.move"].create(
+        cls.move = cls._create_invoice(cls.journal)
+        cls.move.action_post()
+
+        cls.move_sale = cls._create_invoice(cls.journal_sale)
+        cls.move_sale.action_post()
+
+        cls.move_purchase = cls._create_invoice(cls.journal_purchase)
+        cls.move_purchase.action_post()
+
+    @classmethod
+    def _create_invoice(cls, journal):
+        move = cls.env["account.move"].create(
             {
-                "journal_id": cls.journal.id,
+                "journal_id": journal.id,
                 "date": cls.today,
                 "line_ids": [
-                    (0, 0, {"account_id": cls.account_1.id, "name": "/", "debit": 75}),
-                    (0, 0, {"account_id": cls.account_1.id, "name": "/", "debit": 25}),
-                    (
-                        0,
-                        0,
-                        {"account_id": cls.account_2.id, "name": "/", "credit": 100},
-                    ),
+                    (0, 0, {"account_id": cls.account_1.id, "name": "/",
+                            "debit": 75}),
+                    (0, 0, {"account_id": cls.account_1.id, "name": "/",
+                            "debit": 25}),
+                    (0, 0, {"account_id": cls.account_2.id, "name": "/",
+                            "credit": 100},),
                 ],
-            }
-        )
-        cls.move.action_post()
+            })
+        return move
 
     def test_reverse_reversed_entry_fail(self):
         self._reverse_move(self.move)
         with self.assertRaises(UserError):
             self._reverse_move(self.move)
+
+    def test_reverse_reversed_entry_sale_pass(self):
+        self._reverse_move(self.move_sale)
+        assert self._reverse_move(self.move_sale)
+
+    def test_reverse_reversed_entry_purchase_pass(self):
+        self._reverse_move(self.move_purchase)
+        assert self._reverse_move(self.move_purchase)
 
     def test_reverse_reversal_entry_fail(self):
         self._reverse_move(self.move)
@@ -58,7 +86,18 @@ class TestAccountMoveUniqueReversal(common.SavepointCase):
         with self.assertRaises(UserError):
             self._reverse_move(reversal_move)
 
+    def test_reverse_reversal_entry_sale_pass(self):
+        self._reverse_move(self.move_sale)
+        reversal_move = self.move_sale.reversal_move_id
+        assert self._reverse_move(reversal_move)
+
+    def test_reverse_reversal_entry_purchase_pass(self):
+        self._reverse_move(self.move_purchase)
+        reversal_move = self.move_purchase.reversal_move_id
+        assert self._reverse_move(reversal_move)
+
     def _reverse_move(self, move):
         wizard_env = self.env["account.move.reversal"]
-        wizard_env = wizard_env.with_context(active_ids=[move.id], active_model="account.move")
-        wizard_env.create({"date": self.today}).reverse_moves()
+        wizard_env = wizard_env.with_context(active_ids=[move.id],
+                                             active_model="account.move")
+        return wizard_env.create({"date": self.today}).reverse_moves()
