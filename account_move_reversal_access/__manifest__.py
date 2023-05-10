6f2e6e86b2bcadd58e51cbd6cc4263805eafda0d
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-05-10 09:38:59 +0300

    TA#41747 [CHG] account_move_reversal_access : module version

--- account_move_reversal_access/__manifest__.py
+++ account_move_reversal_access/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Move Reversal Access",
     "summary": "Restricting access to the function of reversing Journal Entries and resetting to draft.",
-    "version": "12.0.2.0.0",
+    "version": "14.0.1.0.0",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",
     "maintainer": "Numigi",
