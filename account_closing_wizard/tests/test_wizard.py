46113390999ae20f0d71ccc12c3cd47e05855106
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-05-12 11:35:31 +0300

    TA#53195 [ADD] account_closing_wizard : check before closing fiscal year

--- account_closing_wizard/tests/test_wizard.py
+++ account_closing_wizard/tests/test_wizard.py
@@ -79,8 +79,10 @@ class TestWizard(common.SavepointCase):
                 "code": "50000",
             }
         )
-        cls.move_1 = cls._make_move(cls.receivable_account, cls.revenue_account, 300)
-        cls.move_2 = cls._make_move(cls.expense_account, cls.payable_account, 200)
+        cls.move_1 = cls._make_move(
+            cls.receivable_account, cls.revenue_account, 300)
+        cls.move_2 = cls._make_move(
+            cls.expense_account, cls.payable_account, 200)
 
         cls.wizard = cls.env["account.closing.wizard"].create(
             {
@@ -119,18 +121,17 @@ class TestWizard(common.SavepointCase):
         assert self.wizard._get_default_journal_id() == self.closing_journal
 
     def test_confirm__no_posted_entry(self):
-        self.wizard.confirm()
+        with pytest.raises(ValidationError):
+            self.wizard.confirm()
         move = self.wizard.move_id
-        assert move.date == self.date_to
-        assert move.company_id == self.company
-        assert move.journal_id == self.closing_journal
-        assert move.state == "draft"
-        assert move.is_closing
+        assert move.date != self.date_to
+        assert move.company_id != self.company
+        assert move.journal_id != self.closing_journal
+        assert move.state != "draft"
+        assert move.is_closing is False
 
         lines = move.line_ids
-        assert len(lines) == 1
-        assert lines[0].account_id == self.earnings_account
-        assert lines[0].balance == 0
+        assert len(lines) == 0
 
     def test_confirm__entries_posted(self):
         self.move_1.post()
@@ -148,12 +149,18 @@ class TestWizard(common.SavepointCase):
         assert lines[2].credit == 200
 
     def test_account_move_ref(self):
+        self.move_1.post()
+        self.move_2.post()
         date_from = self.date_from.strftime(DATE_FORMAT)
         date_to = self.date_to.strftime(DATE_FORMAT)
         self.wizard.confirm()
         move = self.wizard.move_id
         assert date_from in move.ref
         assert date_to in move.ref
+
+    def test_account_move_close_draft_in_period(self):
+        with pytest.raises(ValidationError):
+            self.wizard.confirm()
 
     def test_no_earnings_account(self):
         self.earnings_account.is_default_earnings_account = False
