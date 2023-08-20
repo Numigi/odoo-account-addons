bd9d4b6e70099573be44e853858ffe9374b35114
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-08-20 17:26:43 +0300

    TA#53621 [ADD] account_dashboard_onboarding_state : new  module to display banner

---
+++ account_dashboard_onboarding_state/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Dashboard Onboarding State',
+    'version': '1.1.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'license': 'AGPL-3',
+    'category': 'Accounting',
+    'summary': 'Manage the dashboard onboarding state on accounting.',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/res_company.xml',
+    ],
+    'installable': True,
+}
