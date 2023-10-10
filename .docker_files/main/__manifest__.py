6c67985f010d4c61c64b5f83ea59548a1d6c8cc3
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-10-10 09:13:23 +0300

    debug circleci

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -21,7 +21,7 @@
         "account_closing_journal_mis_builder",
         "account_closing_wizard",
         "account_fr_ca_labels",
-        "account_invoice_constraint_chronology_forced",
+        # "account_invoice_constraint_chronology_forced",# Disable when testing account_move_chronology_qa
         "account_move_chronology_qa",
         "account_move_reversal_access",
         "account_move_unique_reversal",
