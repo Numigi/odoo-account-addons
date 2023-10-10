f7cfc33d42a3900aaa2788912ed82607b57569ae
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-10-10 08:40:27 +0300

    TA#57577 [ADD] account_move_chronology_qa : unit tests

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -22,6 +22,7 @@
         "account_closing_wizard",
         "account_fr_ca_labels",
         "account_invoice_constraint_chronology_forced",
+        "account_move_chronology_qa",
         "account_move_reversal_access",
         "account_move_unique_reversal",
         "account_negative_debit_credit",
