7391a5f2d2594b830766e261e0ff5913c992756f
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2024-04-19 06:34:12 +0300

    [UPD] Version updated for test

--- bank_statement_online_stripe/__manifest__.py
+++ bank_statement_online_stripe/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Bank Statement Online Stripe",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://numigi.com",
