4d0ab90af2b8b17bd3b1dbd3844c214f5d550593
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-06-27 16:17:04 +0100

    [16.0][MIG] : bank_statement_import_csv

---
+++ bank_statement_import_csv/error.py
@@ -0,0 +1,15 @@
+# © 2022 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from dataclasses import dataclass, field
+
+
+@dataclass
+class BankStatementError:
+    msg: str
+    args: list = field(default_factory=lambda: [])
+    kwargs: dict = field(default_factory=lambda: {})
+
+
+def is_bank_statement_error(value):
+    return isinstance(value, BankStatementError)
---
+++ bank_statement_import_csv/models/account_journal.py
@@ -0,0 +1,13 @@
+# © 2022 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class AccountJournal(models.Model):
+
+    _inherit = "account.journal"
+
+    statement_import_config_id = fields.Many2one(
+        "bank.statement.import.config",
+    )
