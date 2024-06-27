e20e170e1f3bbe58e513e375692698c74c431a38
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-06-27 11:05:11 +0100

    [16.0][MIG] add account_show_full_features

---
+++ account_show_full_features/__manifest__.py
@@ -0,0 +1,22 @@
+# © 2019 Numigi
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Show Full Accounting Features',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://www.numigi.com',
+    'license': 'LGPL-3',
+    'category': 'Accounting',
+    'summary': 'Show the accounting features hidden in Odoo community',
+    'depends': [
+        'base_xml_rename',
+        'account',
+    ],
+    'data': [
+        'data/ir_ui_menu.xml',
+        'data/res_groups.xml',
+    ],
+    'installable': True,
+}
