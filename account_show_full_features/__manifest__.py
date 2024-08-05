b139443cc23d1bae7e15d0863ffcb06c1a7b276c
Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-08-05 11:24:54 +0300

    Restore simple quotes

--- account_show_full_features/__manifest__.py
+++ account_show_full_features/__manifest__.py
@@ -2,20 +2,20 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    "name": "Show Full Accounting Features",
-    "version": "1.0.1",
-    "author": "Numigi",
-    "maintainer": "Numigi",
-    "website": "https://www.numigi.com",
-    "license": "LGPL-3",
-    "category": "Accounting",
-    "summary": "Show the accounting features hidden in Odoo community",
-    "depends": [
-        "account",
+    'name': 'Show Full Accounting Features',
+    'version': '1.0.1',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://www.numigi.com',
+    'license': 'LGPL-3',
+    'category': 'Accounting',
+    'summary': 'Show the accounting features hidden in Odoo community',
+    'depends': [
+        'account',
     ],
-    "data": [
-        "data/ir_ui_menu.xml",
-        "data/res_groups.xml",
+    'data': [
+        'data/ir_ui_menu.xml',
+        'data/res_groups.xml',
     ],
-    "installable": True,
+    'installable': True,
 }
