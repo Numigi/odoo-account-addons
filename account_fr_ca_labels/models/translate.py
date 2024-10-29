a7144554d662fd6f7ab9c93d05c338c1f3ab8ca9
Rivo Lalaina <rivo2302@gmail.com>
2024-10-29 15:45:05 +0300

    TA#66813 [16.0][MIG] account_fr_ca_labels

--- account_fr_ca_labels/models/translate.py
+++ account_fr_ca_labels/models/translate.py
@@ -1,7 +1,14 @@
 # Copyright 2024 Numigi and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+from odoo import SUPERUSER_ID
 from odoo.tools.translate import TranslationImporter as BaseTranslationImporter
+from odoo.tools.translate import GettextAlias as BaseGettextAlias
+
+import odoo
+
+_base_load_translation = BaseTranslationImporter._load
+_base_get_translation = BaseGettextAlias._get_translation
 
 
 def replace_vals(data, mapping, lang="fr_FR"):
@@ -24,17 +31,14 @@ def replace_vals(data, mapping, lang="fr_FR"):
     return data
 
 
-original_load = BaseTranslationImporter._load
-
-
 class TranslationImporter(BaseTranslationImporter):
 
-    def load_terms(self, reader, lang, xmlids=None):
+    def _load(self, reader, lang, xmlids=None):
         """
         Load and apply language-specific term replacements.
         """
 
-        original_load(self, reader, lang, xmlids)
+        _base_load_translation(self, reader, lang, xmlids)
 
         if lang == "fr_FR":
             term_model = "translate.term.fr_ca"
@@ -56,4 +60,34 @@ class TranslationImporter(BaseTranslationImporter):
                 )
 
 
-BaseTranslationImporter._load = TranslationImporter.load_terms
+class GettextAlias(BaseGettextAlias):
+    def _get_translation(self, source, module=None):
+        res = _base_get_translation(self, source, module=None)
+        db = self._get_db()
+        cr = False
+        if db is not None:
+            cr = db.cursor()
+        if cr:
+            env = odoo.api.Environment(cr, SUPERUSER_ID, {})
+            lang = env["res.users"].context_get()["lang"]
+            if lang == "fr_FR":
+                term_model = "translate.term.fr_ca"
+                mapping_dict = (
+                    {
+                        record.term_fr: record.term_ca
+                        for record in env[term_model].search([])
+                    }
+                    if term_model in env.registry.models
+                    else {}
+                )
+
+                for old, new in mapping_dict.items():
+                    if old in res:
+                        res = res.replace(old, new)
+                        break
+
+        return res
+
+
+BaseTranslationImporter._load = TranslationImporter._load
+BaseGettextAlias._get_translation = GettextAlias._get_translation
