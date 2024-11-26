a1acec8bc9bf821f689e7f62989cf8f773caef72
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2024-11-26 21:13:03 +0100

    Update test_account_check_deposit.py

--- account_check_deposit_enhanced/tests/test_account_check_deposit.py
+++ account_check_deposit_enhanced/tests/test_account_check_deposit.py
@@ -74,7 +74,7 @@ class TestAccountCheckDeposit(SavepointCase):
 
     def test_debit_move_line_has_company(self):
         self.deposit.validate_deposit()
-        debit = self.deposit.line_ids.filtered(lambda l: l.credit)
+        debit = self.deposit.check_payment_ids.filtered(lambda l: l.debit)
         assert debit.partner_id == self.env.user.company_id.partner_id
-        credit = self.deposit.line_ids.filtered(lambda l: l.credit)
+        credit = self.deposit.check_payment_ids.filtered(lambda l: l.credit)
         assert credit.partner_id == self.partner
