22f774ce9f00b84246e1c97940d3df51aca9a9df
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2024-11-27 17:25:02 +0100

    TA#72332 [14.0][FIX] account_check_deposit_enhanced (#231)
    - TA#72332 [14.0][FIX] account_check_deposit_enhanced

--- account_check_deposit_enhanced/models/account_check_deposit.py
+++ account_check_deposit_enhanced/models/account_check_deposit.py
@@ -1,14 +1,15 @@
 # © 2022 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class AccountCheckDeposit(models.Model):
     _inherit = "account.check.deposit"
 
-    @api.model
-    def _prepare_counterpart_move_lines_vals(self, *args, **kwargs):
-        vals = super()._prepare_counterpart_move_lines_vals(*args, **kwargs)
-        vals["partner_id"] = self.company_id.partner_id.id
+    def _prepare_move_vals(self):
+        vals = super()._prepare_move_vals()
+        for line in vals["line_ids"]:
+            if 'debit' in line[2].keys():
+                line[2]['partner_id'] = self.company_id.partner_id.id
         return vals
