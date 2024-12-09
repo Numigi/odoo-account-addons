794125590780ef89cb4f1767be248d2faddadca7
Rivo Lalaina <rivo2302@gmail.com>
2024-12-09 21:38:04 +0300

    [IMP][16.0] pre-commit : add black hook (#232)

--- account_invoice_constraint_chronology_forced/__manifest__.py
+++ account_invoice_constraint_chronology_forced/__manifest__.py
@@ -2,16 +2,16 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Account Invoice Constraint Chronology Forced',
-    'version': '1.1.0',
-    'author': 'Numigi',
-    'maintainer': 'Numigi',
-    'website': 'https://www.numigi.com',
-    'license': 'LGPL-3',
-    'category': 'Accounting',
-    'summary': 'Add menu entries to configure bank accounts and banks of partners',
-    'depends': [
-        'account_invoice_constraint_chronology',
+    "name": "Account Invoice Constraint Chronology Forced",
+    "version": "1.1.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "LGPL-3",
+    "category": "Accounting",
+    "summary": "Add menu entries to configure bank accounts and banks of partners",
+    "depends": [
+        "account_invoice_constraint_chronology",
     ],
     "data": ["views/account_journal.xml"],
     "installable": True,
--- account_search_date_range/tests/test_fiscal_year.py
+++ account_search_date_range/tests/test_fiscal_year.py
@@ -9,17 +9,16 @@ from odoo.tests import common
 
 @ddt
 class TestSessionInfo(common.TransactionCase):
-
     @data(
-        ('2018-01-01', 12, 31, '2018-01-01'),
-        ('2018-05-15', 12, 31, '2018-01-01'),
-        ('2018-12-31', 12, 31, '2018-01-01'),
-        ('2018-02-28', 2, 29, '2017-03-01'),
-        ('2018-03-01', 2, 29, '2018-03-01'),
-        ('2018-12-31', 2, 29, '2018-03-01'),
-        ('2018-02-01', 11, 30, '2017-12-01'),
-        ('2018-03-01', 11, 30, '2017-12-01'),
-        ('2018-12-31', 11, 30, '2018-12-01'),
+        ("2018-01-01", 12, 31, "2018-01-01"),
+        ("2018-05-15", 12, 31, "2018-01-01"),
+        ("2018-12-31", 12, 31, "2018-01-01"),
+        ("2018-02-28", 2, 29, "2017-03-01"),
+        ("2018-03-01", 2, 29, "2018-03-01"),
+        ("2018-12-31", 2, 29, "2018-03-01"),
+        ("2018-02-01", 11, 30, "2017-12-01"),
+        ("2018-03-01", 11, 30, "2017-12-01"),
+        ("2018-12-31", 11, 30, "2018-12-01"),
     )
     @unpack
     def test_fiscal_year_start(self, today, last_month, last_day, year_start):
@@ -28,26 +27,26 @@ class TestSessionInfo(common.TransactionCase):
 
         self.env.user.company_id.write(
             {
-                'fiscalyear_last_month': str(last_month),
-                'fiscalyear_last_day': str(last_day),
+                "fiscalyear_last_month": str(last_month),
+                "fiscalyear_last_day": str(last_day),
             }
         )
 
         with freeze_time(today):
             context = self._get_domain_context()
-            assert context['fiscal_year_start'] == year_start
+            assert context["fiscal_year_start"] == year_start
 
     @data(
-        ('2018-01-01', 12, 31, '2018-01-01'),
-        ('2018-05-15', 12, 31, '2018-04-01'),
-        ('2018-12-31', 12, 31, '2018-10-01'),
-        ('2018-02-01', 2, 29, '2017-12-01'),
-        ('2018-03-01', 2, 29, '2018-03-01'),
-        ('2018-12-31', 2, 29, '2018-12-01'),
-        ('2018-11-30', 11, 30, '2018-09-01'),
-        ('2018-03-01', 11, 30, '2018-03-01'),
-        ('2018-12-31', 11, 30, '2018-12-01'),
-        ('2018-07-15', 11, 30, '2018-06-01'),
+        ("2018-01-01", 12, 31, "2018-01-01"),
+        ("2018-05-15", 12, 31, "2018-04-01"),
+        ("2018-12-31", 12, 31, "2018-10-01"),
+        ("2018-02-01", 2, 29, "2017-12-01"),
+        ("2018-03-01", 2, 29, "2018-03-01"),
+        ("2018-12-31", 2, 29, "2018-12-01"),
+        ("2018-11-30", 11, 30, "2018-09-01"),
+        ("2018-03-01", 11, 30, "2018-03-01"),
+        ("2018-12-31", 11, 30, "2018-12-01"),
+        ("2018-07-15", 11, 30, "2018-06-01"),
     )
     @unpack
     def test_trimester_start(self, today, last_month, last_day, year_start):
@@ -56,14 +55,14 @@ class TestSessionInfo(common.TransactionCase):
 
         self.env.user.company_id.write(
             {
-                'fiscalyear_last_month': str(last_month),
-                'fiscalyear_last_day': str(last_day),
+                "fiscalyear_last_month": str(last_month),
+                "fiscalyear_last_day": str(last_day),
             }
         )
 
         with freeze_time(today):
             context = self._get_domain_context()
-            assert context['trimester_start'] == year_start
+            assert context["trimester_start"] == year_start
 
     def _get_domain_context(self):
         return self.env["search.date.range"]._get_domain_context("date")
