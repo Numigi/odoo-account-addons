d68240dac4a9f8907bf7b8770d56fff1954960c7
Rivo Lalaina <rivo2302@gmail.com>
2025-02-04 16:20:03 +0300

    [14.0][ADD] account_move_supplier_ref_required

---
+++ account_move_supplier_ref_required/README.rst
@@ -0,0 +1,18 @@
+Account Move Supplier Ref Required
+==================================
+
+This module makes the ref field on account.move required from the form view.
+
+.. image:: static/description/account_move_form.png
+
+Configuration
+-------------
+No configuration required apart from module installation.
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+
+More information
+----------------
+* Meet us at https://bit.ly/numigi-com
---
+++ account_move_supplier_ref_required/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Account Move Supllier Ref Required",
+    "summary": "",
+    "version": "14.0.1.0.1",
+    "website": "https://bit.ly/numigi-com",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+}
