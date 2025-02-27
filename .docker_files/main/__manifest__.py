2dec5695047357a7ad4c9ec7c9c9ce66eebfae74
Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
2025-02-27 06:37:40 +0300

    TA#72072 [16.0][MIG] account_manual_entry_restricted

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -16,6 +16,7 @@
         "account_fiscalyear_end_on_company",
         "account_fr_ca_labels",
         "account_invoice_constraint_chronology_forced",
+        "account_manual_entry_restricted",
         "account_move_reversal_access",
         "account_move_unique_reversal",
         "account_negative_debit_credit",
---
+++ account_manual_entry_restricted/models/account_move.py
@@ -0,0 +1,47 @@
+# Copyright 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import _, models
+from odoo.exceptions import ValidationError
+
+
+class JournalEntry(models.Model):
+
+    _inherit = "account.move"
+
+    def _check_manual_entry_restrictions(self):
+        restricted_accounts = self.mapped("line_ids.account_id").filtered(
+            lambda a: a.manual_entry_group_ids
+        )
+
+        for account in restricted_accounts:
+            allowed_groups = account.manual_entry_group_ids
+            user_is_forbidden = not (allowed_groups & self.env.user.groups_id)
+            if user_is_forbidden:
+                raise ValidationError(
+                    _(
+                        "Access right error:\n\n"
+                        "Only the following user group(s) are authorized to post "
+                        "in the account {account}:\n\n{groups}\n\n"
+                        "Please contact your administrator."
+                    ).format(
+                        account=account.display_name,
+                        groups="\n".join(
+                            ["- {}".format(g.display_name) for g in allowed_groups]
+                        ),
+                    )
+                )
+
+    def action_post(self):
+        """Verify the contrains on manual journal entries.
+
+        This method is a wrapper around the method post.
+
+        It is only called by the button from the form
+        view of account.move.
+
+        The method post is called directly for automatted entries
+        (invoices, payments, expenses, etc).
+        """
+        self._check_manual_entry_restrictions()
+        return super().action_post()
