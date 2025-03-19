69b3060a99639794b43beeb1f9120e6fb2648f15
Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
2025-03-19 13:37:28 +0300

    TA#72074 [16.0][MIG] account_move_reversed_entry

---
+++ account_move_reversed_entry/tests/test_account_move.py
@@ -0,0 +1,45 @@
+# Copyright 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields
+from odoo.tests import common
+
+
+class TestAccountMove(common.TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.journal = cls.env["account.journal"].search([], limit=1)
+        cls.account_1 = cls.env["account.account"].create(
+            {
+                "name": "Account 1",
+                "code": "501001",
+                "reconcile": True,
+            }
+        )
+        cls.account_2 = cls.env["account.account"].create(
+            {
+                "name": "Account 2",
+                "code": "101001",
+                "reconcile": True,
+            }
+        )
+        cls.move = cls.env["account.move"].create(
+            {
+                "journal_id": cls.journal.id,
+                "line_ids": [
+                    (0, 0, {"account_id": cls.account_1.id, "name": "/", "debit": 100}),
+                    (
+                        0,
+                        0,
+                        {"account_id": cls.account_2.id, "name": "/", "credit": 100},
+                    ),
+                ],
+            }
+        )
+        cls.today = fields.date.today()
+
+    def test_reversed_entry(self):
+        self.move._reverse_moves()
+        reversal_entry = self.move.reverse_entry_id
+        assert reversal_entry.reversed_entry_id == self.move
