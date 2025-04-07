d184cdb0f564454e9c75dda111d7c0ce40ef08bb
Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
2025-04-07 14:45:45 +0300

    TA#76803 [14.0][MIG] chekc_vat_ca

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -53,6 +53,7 @@
         "bank_statement_partner_name",
         "canada_account_types",
         "canada_bank_transfer",
+        "check_vat_ca",
         "hr_expense_tax_adjustment",
         "invoice_currency_validation",
         # FIX ME: Conflict with UT of account_invoice_constraint_chronology OCA Module
---
+++ check_vat_ca/tests/test_partner_vat_validation.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import SavepointCase
+
+
+class TestPartnerVatValidation(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def test_check_vat_canada(self):
+        partner = self.env["res.partner"].create(
+            {
+                "name": "Test Canadian Partner",
+                "country_id": self.env.ref("base.ca").id,
+            }
+        )
+        # The constrainte will not be raised even if the vat does't contain "CA"
+        partner.vat = "123456789"
