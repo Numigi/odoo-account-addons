65f1417f541b0cf00bc7d22d0588bdad6dfc1d9e
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2025-04-15 11:57:47 +0100

    TA#76453[ADD] : account_move_recompute_amount_currency

---
+++ account_move_recompute_amount_currency/models/account_move.py
@@ -0,0 +1,16 @@
+# © 2025 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.model
+    def _get_fields_onchange_subtotal_model(self, price_subtotal, move_type, currency,
+                                            company, date):
+        res = super()._get_fields_onchange_subtotal_model(price_subtotal, move_type,
+            currency, company, date)
+        res['amount_currency'] = 0.0
+        return res
