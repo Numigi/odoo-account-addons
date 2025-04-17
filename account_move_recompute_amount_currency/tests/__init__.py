109679547e6683509b05636355252236be914ebe
Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
2025-04-17 15:59:27 +0300

    Code quality

--- account_move_recompute_amount_currency/tests/__init__.py
+++ account_move_recompute_amount_currency/tests/__init__.py
@@ -1,4 +1,4 @@
-# © 2025 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# Copyright 2025 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import test_invoice_recompute_currency_amount
