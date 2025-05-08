a510073093786f19310fc8efbd4f92613febe1ca
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2025-05-08 16:58:15 +0100

    TA#77199 [IMP] canada_bank_transfer : add skip_account_move_synchronization to change_payment_date (#267)
    * TA#77199 [IMP] canada_bank_transfer : add skip_account_move_synchronization to change_payment_date
    
    * TA#77199 [IMP] canada_bank_transfer : add skip_account_move_synchronization to change_payment_date
    
    * TA#77199 [IMP] canada_bank_transfer : add skip_account_move_synchronization to change_payment_date

--- canada_bank_transfer/__manifest__.py
+++ canada_bank_transfer/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Canada Bank Transfer",
-    "version": "1.4.0",
+    "version": "14.0.1.5.0",
     "author": "Savoir-faire Linux,Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
