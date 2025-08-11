e42f6a56b83f67f050a6232149e0fbbd4c4f7123
Lanto Razafindrabe <124650562+lanto-razafindrabe@users.noreply.github.com>
2025-08-11 21:49:38 +0300

    TA#78963 [16.0][MIG][CHG] account_additional_settings (#281)
    * Update Dockerfile
    
    * TA#78963 [16.0][MIG][CHG] account_additional_settings

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -12,6 +12,7 @@
     "summary": "Install all addons required for testing.",
     "depends": [
         "account",
+        "account_additional_settings",
         "account_bank_menu",
         "account_fiscalyear_end_on_company",
         "account_fr_ca_labels",
