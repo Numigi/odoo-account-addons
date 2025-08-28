02b3be3c6c873108f70dd86cf73f403c9f83cff8
Lanto Razafindrabe <124650562+lanto-razafindrabe@users.noreply.github.com>
2025-08-28 16:49:12 +0300

    TA#78820 [14.0][ADD][FIX] canada_bank_transfer  : using transit account (#284)
    * TA#78820 [14.0][ADD][FIX] canada_bank_transfer  : using transit account
    
    * Update eft_confirmation.py

--- canada_bank_transfer/__manifest__.py
+++ canada_bank_transfer/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Canada Bank Transfer",
-    "version": "14.0.1.5.0",
+    "version": "14.0.1.6.0",
     "author": "Savoir-faire Linux,Numigi",
     "maintainer": "Numigi",
     "website": "https://numigi.com/r/home",
