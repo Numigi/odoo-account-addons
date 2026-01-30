2503e84f37a90fb53d5b5fdbc5e523e0ed0160c6
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2026-01-30 17:22:28 +0100

    TA#82390 [IMP] account_financial_ageing_date wizard UI (#296)
    * TA#82390 [IMP] account_financial_ageing_date
    
    * TA#82390 [IMP] account_financial_ageing_date wizard UI
    
    * TA#82390 [IMP] account_financial_ageing_date wizard UI
    
    ---------
    
    Co-authored-by: Abdellatif Benzbiria <odoo-base@numigi.com>

--- account_financial_report_ageing_date/wizard/aged_partner_balance_wizard.py
+++ account_financial_report_ageing_date/wizard/aged_partner_balance_wizard.py
@@ -1,7 +1,7 @@
 # Copyright 2026 Numigi
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import models, fields, api, _
 
 
 class AgedPartnerBalanceWizard(models.TransientModel):
@@ -18,6 +18,20 @@ class AgedPartnerBalanceWizard(models.TransientModel):
         required=True,
         help="Choose 'Due Date' for collection analysis or 'Invoice Date' for financial analysis."
     )
+
+    @api.onchange('ageing_method', 'show_move_line_details')
+    def _onchange_ageing_method(self):
+        """ Prevent selecting 'Invoice Date' if details are shown. """
+        if self.ageing_method == 'date' and self.show_move_line_details:
+            self.ageing_method = 'date_due'
+            return {
+                'warning': {
+                    'title': _('Incompatible Options'),
+                    'message': _('The "Invoice Date" ageing method is not compatible with "Show Move Line Details". '
+                                 'Please disable details to use this method. '
+                                 'The method has been reset to "Due Date".')
+                }
+            }
 
     def _prepare_report_aged_partner_balance(self):
         """ Override to pass the ageing_method to the transient report model. """
