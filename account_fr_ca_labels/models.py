2ae278bbdc50407356c731bfbf7575e2f5ec805a
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-02-02 19:27:19 +0100

    TA#82714[18.0][IMP] dockerfile

--- account_fr_ca_labels/models.py
+++ account_fr_ca_labels/models.py
@@ -101,7 +101,7 @@ def _update_reconciliation_translations(env):
         ("Non lettré", "Non réconcilié"),
         ("Lettrer", "Réconcilier"),
         ("lettrer", "réconcilier"),
-        ("Conciliation","Lettrage"),
+        ("Lettrage", "Conciliation"),
         # Grammatical errors
         ("annuler le lettrage l'entrée", "annuler la conciliation"),
     ]
