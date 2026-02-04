e2abc5b27dc43b131a38e5faa5609daf66a7d2d6
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-02-04 16:31:55 +0100

    TA#82714 [IMP] _update_reconciliation_translations

--- account_fr_ca_labels/models.py
+++ account_fr_ca_labels/models.py
@@ -95,14 +95,13 @@ def _update_reconciliation_translations(env):
     """Update the term `Lettrage` to `Conciliation`."""
     mapping = [
         ("Modèles de lettrage", "Modèles de conciliation bancaire"),
-        ("Modèles de rapprochement", "Modèles de conciliation bancaire"),
         ("de lettrage", "de conciliation"),
         ("du lettrage", "de la conciliation"),
         ("le lettrage", "la conciliation"),
-        ("Rapprochement", "Conciliation"),
-        ("Lettrage", "Conciliation"),
+        ("Non lettré", "Non réconcilié"),
         ("Lettrer", "Réconcilier"),
-        ("Rapprocher", "Réconcilier"),
+        ("lettrer", "réconcilier"),
+        ("Lettrage", "Conciliation"),
         # Grammatical errors
         ("annuler le lettrage l'entrée", "annuler la conciliation"),
     ]
