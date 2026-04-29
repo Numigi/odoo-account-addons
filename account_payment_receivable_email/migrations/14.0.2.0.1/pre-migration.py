95e2a7eee3e94ac0601e5f11948830af88dfaa9b
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2026-04-29 18:16:56 +0000

    [ADD] account_payement_receivable_email migration script logging (#314)
    Co-authored-by: Abdellatif Benzbiria <odoo-base@numigi.com>

--- account_payment_receivable_email/migrations/14.0.2.0.1/pre-migration.py
+++ account_payment_receivable_email/migrations/14.0.2.0.1/pre-migration.py
@@ -1,9 +1,18 @@
 # © Numigi (tm) and all its contributors (https://numigi.com/r/home)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import logging
+_logger = logging.getLogger(__name__)
+
+
 def migrate(cr, version):
     """
-    This script runs BEFORE the model is updated by the ORM.
-    It pre-creates the column to avoid the “column does not exist” error
+    This script runs BEFORE the model update by the ORM.
+    It pre-creates the column to avoid the "column does not exist" error
     when the ORM reads res.partner prematurely.
     """
+    _logger.info("START PRE-MIGRATION: Checking/Creating column "
+                 "'payment_email_id' in 'res_partner'...")
+
     cr.execute("ALTER TABLE res_partner ADD COLUMN IF NOT EXISTS payment_email_id integer;")
+
+    _logger.info("END PRE-MIGRATION: Column 'payment_email_id' is ready for the ORM.")
