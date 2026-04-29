c495d3c4427bc079f04d27569acd82c44d6248f6
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2026-04-29 16:20:55 +0000

    Upate branch (#312)
    * TA#82816  [14.0][IMP] account_payment_receivable_email : manage payment_email_id (#310)
    
    * [14.0][IMP] account_payment_receivable_email : manage payment_email_id
    
    
    ---------
    
    Co-authored-by: Abdellatif Benzbiria <odoo-base@numigi.com>
    
    * TA#82816 Update res_partner.xml (#311)
    
    * Update res_partner.xml
    
    * Update res_partner.xml
    
    ---------
    
    Co-authored-by: Abdellatif Benzbiria <odoo-base@numigi.com>

--- account_payment_receivable_email/__manifest__.py
+++ account_payment_receivable_email/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Account Payment Receivable Email',
-    'version': '14.0.2.1.0',
+    'version': '14.0.2.0.0',
     'category': 'Accounting',
     'author': 'Numigi',
     "maintainer": "Numigi",
