3c24bc445caddbe3d56bfcfd6218e0d9b1d4b1aa
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-05-05 10:40:55 +0000

    [18.0][MIG] account_move_unique_reversal

---
+++ account_move_unique_reversal/tests/test_account_move_unique_reversal.py
@@ -0,0 +1,114 @@
+# © Numigi (tm) and all its contributors (https://numigi.com/r/home)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields
+from odoo.exceptions import UserError
+from odoo.tests import common
+
+
+class TestAccountMoveUniqueReversal(common.TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.journal = cls.env["account.journal"].create(
+            {"name": "Test", "code": "TEST", "type": "general"}
+        )
+        cls.journal_sale = cls.env["account.journal"].create(
+            {
+                "name": "Test Sales Journal",
+                "code": "tSAL",
+                "type": "sale",
+            }
+        )
+        cls.journal_purchase = cls.env["account.journal"].create(
+            {
+                "name": "Test Purchase Journal",
+                "code": "tPO",
+                "type": "purchase",
+            }
+        )
+        cls.today = fields.Date.today()
+
+        cls.account_1 = cls.env["account.account"].create(
+            {
+                "name": "Account 1",
+                "code": "501001",
+                "account_type": "expense",  # Remplacement V18
+            }
+        )
+        cls.account_2 = cls.env["account.account"].create(
+            {
+                "name": "Account 2",
+                "code": "101001",
+                "account_type": "asset_fixed",  # Remplacement V18
+            }
+        )
+        cls.move = cls._create_invoice(cls.journal)
+        cls.move.action_post()
+
+        cls.move_sale = cls._create_invoice(cls.journal_sale)
+        cls.move_sale.action_post()
+
+        cls.move_purchase = cls._create_invoice(cls.journal_purchase)
+        cls.move_purchase.action_post()
+
+    @classmethod
+    def _create_invoice(cls, journal):
+        move = cls.env["account.move"].create(
+            {
+                "journal_id": journal.id,
+                "date": cls.today,
+                "line_ids": [
+                    (0, 0, {"account_id": cls.account_1.id, "name": "/", "debit": 75}),
+                    (0, 0, {"account_id": cls.account_1.id, "name": "/", "debit": 25}),
+                    (
+                        0,
+                        0,
+                        {"account_id": cls.account_2.id, "name": "/", "credit": 100},
+                    ),
+                ],
+            }
+        )
+        return move
+
+    def test_reverse_reversed_entry_fail(self):
+        self._reverse_move(self.move)
+        with self.assertRaises(UserError):
+            self._reverse_move(self.move)
+
+    def test_reverse_reversed_entry_sale_pass(self):
+        self._reverse_move(self.move_sale)
+        assert self._reverse_move(self.move_sale)
+
+    def test_reverse_reversed_entry_purchase_pass(self):
+        self._reverse_move(self.move_purchase)
+        assert self._reverse_move(self.move_purchase)
+
+    def test_reverse_reversal_entry_fail(self):
+        self._reverse_move(self.move)
+        # Odoo 18: on récupère le premier élément de la liste reversal_move_ids
+        reversal_move = self.move.reversal_move_ids[0]
+        with self.assertRaises(UserError):
+            self._reverse_move(reversal_move)
+
+    def test_reverse_reversal_entry_sale_pass(self):
+        self._reverse_move(self.move_sale)
+        reversal_move = self.move_sale.reversal_move_ids[0]
+        assert self._reverse_move(reversal_move)
+
+    def test_reverse_reversal_entry_purchase_pass(self):
+        self._reverse_move(self.move_purchase)
+        reversal_move = self.move_purchase.reversal_move_ids[0]
+        assert self._reverse_move(reversal_move)
+
+    def _reverse_move(self, move):
+        wizard_env = self.env["account.move.reversal"].with_context(
+            active_ids=move.ids, active_model="account.move"
+        )
+        wizard = wizard_env.create(
+            {
+                "date": self.today,
+                "journal_id": move.journal_id.id,
+            }
+        )
+        return wizard.reverse_moves()
