a995ec770653073a7044d09a0d8642edc1b859ef
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-05-05 10:30:22 +0000

    [18.0][MIG] account_move_unique_reversal

---
+++ account_move_unique_reversal/__manifest__.py
@@ -0,0 +1,15 @@
+# © Numigi (tm) and all its contributors (https://numigi.com/r/home)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Move Unique Reversal",
+    "summary": "Blocks the users from reversing the Journal Entries if they have "
+    "already been reversed or they are the reversal of the other Entries",
+    "version": "18.0.1.0.1",
+    "website": "https://numigi.com/r/home",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "installable": True,
+}
---
+++ account_move_unique_reversal/models/account_move.py
@@ -0,0 +1,34 @@
+# © Numigi (tm) and all its contributors (https://numigi.com/r/home)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, models
+from odoo.exceptions import UserError
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _reverse_moves(self, default_values_list=None, cancel=False):
+        for move in self:
+            if move.reversal_move_ids and move.journal_id.type not in (
+                "sale",
+                "purchase",
+            ):
+                reversals_name = ", ".join(move.reversal_move_ids.mapped('display_name'))
+                raise UserError(
+                    _(
+                        "The accounting entry {} is already reversed (by entry {}). "
+                        "You can only reverse an accounting entry once."
+                    ).format(move.display_name, reversals_name)
+                )
+            if move.reversed_entry_id and move.journal_id.type not in (
+                "sale",
+                "purchase",
+            ):
+                raise UserError(
+                    _(
+                        "The accounting entry {} is the reversal of another entry "
+                        "({}). You can not reverse a reversal accounting entry."
+                    ).format(move.display_name, move.reversed_entry_id.display_name)
+                )
+        return super()._reverse_moves(default_values_list, cancel)
