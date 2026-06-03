511ec82e3fb5b7c5e75710bbf4e60de86536b753
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-06-03 15:23:34 +0000

    [18.0][MIG] invoice_refund_not_earlier

--- invoice_refund_not_earlier/__manifest__.py
+++ invoice_refund_not_earlier/__manifest__.py
@@ -1,6 +1,8 @@
 # Copyright 2023-today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+# pylint: disable=pointless-statement
+# noqa: B018
 {
     "name": "Invoice Refund Not Ealier",
     "version": "18.0.1.0.0",
