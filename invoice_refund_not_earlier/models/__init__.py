be3263fb51b714ab24575c68b6290c51a140fcc1
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-06-03 15:27:28 +0000

    [18.0][MIG] invoice_refund_not_earlier

--- invoice_refund_not_earlier/models/__init__.py
+++ invoice_refund_not_earlier/models/__init__.py
@@ -1,4 +1,6 @@
 # Copyright 2023-today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+"""Init models."""
+
 from . import account_move  # noqa: F401
