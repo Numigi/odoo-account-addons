0b9a17c58571f1f329f8334fb50de90b03b46572
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-06-11 11:38:44 +0000

    [18.0][MIG] account_fiscalyear_end_on_company

--- account_fiscalyear_end_on_company/__manifest__.py
+++ account_fiscalyear_end_on_company/__manifest__.py
@@ -1,6 +1,9 @@
 # Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+# pylint: disable=pointless-statement
+# noqa: B018
+
 {
     "name": "Account Fiscalyear End on Company",
     "version": "18.0.1.0.0",
