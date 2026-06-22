535b63d4c1f1b94b5dc41218d06090cf02bbb5ae
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-06-22 14:11:31 +0000

    [18.0][ADD] account_journal_bank_exclusivity

--- account_journal_bank_exclusivity/models/__init__.py
+++ account_journal_bank_exclusivity/models/__init__.py
@@ -1,5 +1,7 @@
 # Copyright 2026-today Numigi and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+"""Initialize the models for the module."""
+
 from . import account_journal  # noqa: F401
 from . import account_payment_method_line  # noqa: F401
