2752f658ded64f66506699f5c3785c98780f1414
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2026-06-22 15:40:13 +0000

    TA#84732 [18.0][ADD] account_move_block_draft_payment (#340)
    * [ADD] accoubt_move_secure
    
    * [18.0][ADD] account_move_block_draft_payment
    
    * [18.0][ADD] account_move_block_draft_payment
    
    ---------
    
    Co-authored-by: Abdellatif Benzbiria <odoo-base@numigi.com>

---
+++ account_move_draft_payment_block/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2026-today Numigi and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+"""Initialize the account_journal_bank_exclusivity module."""
+
+from . import models  # noqa: F401
---
+++ account_move_draft_payment_block/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026-today Numigi and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+# pylint: disable=pointless-statement
+# noqa: B018
+
+{
+    "name": "Account Move Draft Payment Block",
+    "summary": "Blocks payment registration for draft or cancelled invoices.",
+    "version": "18.0.1.0.0",
+    "category": "Accounting",
+    "author": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "installable": True,
+    "application": False,
+}
