6cac8ba3179ea1cf7b1d4e1545eae7be75feb7fe
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-06-23 11:17:14 +0000

    [18.0][IMP] account_move_draft_payment_block

--- account_move_draft_payment_block/tests/test_account_move_draft_payment_block.py
+++ account_move_draft_payment_block/tests/test_account_move_draft_payment_block.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2026 Numigi and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -13,7 +14,8 @@ class TestAccountMoveBlockDraftPayment(common.TransactionCase):
         self.partner = self.env["res.partner"].create({"name": "Test Partner"})
         self.product = self.env["product.product"].create({"name": "Test Product"})
 
-    def _create_draft_invoice(self):
+    def _create_invoice(self):
+        # Creates a standard draft invoice (also valid for down payments logic)
         return self.env["account.move"].create(
             {
                 "move_type": "out_invoice",
@@ -24,27 +26,64 @@ class TestAccountMoveBlockDraftPayment(common.TransactionCase):
             }
         )
 
-    def _create_posted_invoice(self):
-        invoice = self._create_draft_invoice()
+    def _post_invoice(self, invoice):
         invoice.action_post()
         return invoice
 
-    def _create_cancelled_invoice(self):
-        invoice = self._create_draft_invoice()
-        invoice.button_cancel()
-        return invoice
+    def _register_payment(self, invoice):
+        # Simulates the payment registration wizard
+        payment_register = (
+            self.env["account.payment.register"]
+            .with_context(
+                active_model="account.move",
+                active_ids=invoice.ids,
+            )
+            .create({})
+        )
+        payment_register._create_payments()
 
     def test_payment_on_draft_invoice_raises_error(self):
-        invoice = self._create_draft_invoice()
+        invoice = self._create_invoice()
         with pytest.raises(ValidationError):
             invoice.action_force_register_payment()
 
     def test_payment_on_cancelled_invoice_raises_error(self):
-        invoice = self._create_cancelled_invoice()
+        invoice = self._create_invoice()
+        invoice.button_cancel()
         with pytest.raises(ValidationError):
             invoice.action_force_register_payment()
 
     def test_payment_on_posted_invoice_succeeds(self):
-        invoice = self._create_posted_invoice()
+        invoice = self._create_invoice()
+        self._post_invoice(invoice)
         action_result = invoice.action_force_register_payment()
         assert action_result["res_model"] == "account.payment.register"
+
+    def test_payment_creation_on_posted_invoice(self):
+        invoice = self._create_invoice()
+        self._post_invoice(invoice)
+        self._register_payment(invoice)
+        assert invoice.payment_state in ("in_payment", "paid")
+
+    def test_reset_to_draft_without_payment_succeeds(self):
+        invoice = self._create_invoice()
+        self._post_invoice(invoice)
+        invoice.button_draft()
+        assert invoice.state == "draft"
+
+    def test_reset_to_draft_with_payment_raises_error(self):
+        invoice = self._create_invoice()
+        self._post_invoice(invoice)
+        self._register_payment(invoice)
+
+        with pytest.raises(ValidationError):
+            invoice.button_draft()
+
+    def test_reset_to_draft_with_blocked_payment_state_succeeds(self):
+        # An invoice manually flagged as 'blocked' has no associated payment;
+        # resetting it to draft must remain allowed.
+        invoice = self._create_invoice()
+        self._post_invoice(invoice)
+        invoice.payment_state = "blocked"
+        invoice.button_draft()
+        assert invoice.state == "draft"
