5b7c0a9cf6185e5ccc682a8ad48eaa927c4c8e15
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2026-06-23 11:33:31 +0000

    TA#84732 [18.0][IMP] account_move_draft_payment_block (#342)
    * [ADD] accoubt_move_secure
    
    * [18.0][ADD] account_move_block_draft_payment
    
    * [18.0][ADD] account_move_block_draft_payment
    
    * [18.0][ADD] account_move_block_draft_payment
    
    * [18.0][IMP] account_move_draft_payment_block
    
    ---------
    
    Co-authored-by: Abdellatif Benzbiria <odoo-base@numigi.com>

--- account_move_draft_payment_block/models/account_move.py
+++ account_move_draft_payment_block/models/account_move.py
@@ -13,9 +13,25 @@ class AccountMove(models.Model):
         self._verify_moves_are_posted()
         return super().action_force_register_payment()
 
+    def button_draft(self):
+        self._verify_no_linked_payments()
+        return super().button_draft()
+
     def _verify_moves_are_posted(self):
         unposted_moves = self.filtered(lambda m: m.state != "posted")
         if unposted_moves:
             raise ValidationError(
                 _("You cannot register a payment for an unposted invoice.")
+            )
+
+    def _verify_no_linked_payments(self):
+        invoices = self.filtered(lambda m: m.is_invoice())
+        invoices_with_payments = invoices.filtered(lambda m: m.matched_payment_ids)
+        if invoices_with_payments:
+            raise ValidationError(
+                _(
+                    "It is impossible to reset this invoice to draft because "
+                    "one or more payments are associated with it. "
+                    "Please cancel the concerned payments first."
+                )
             )
