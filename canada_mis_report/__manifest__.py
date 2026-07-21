96b9f3a30867cb5e0ded4053b7f83a20570c1a77
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2026-07-21 17:19:20 +0000

    [IMP]  canada_mis_report  SDV (#359)

--- canada_mis_report/__manifest__.py
+++ canada_mis_report/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Canada MIS Builder Reports",
-    "version": "18.0.1.3.0",
+    "version": "18.0.1.4.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "license": "LGPL-3",
