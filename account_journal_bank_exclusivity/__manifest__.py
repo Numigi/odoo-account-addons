12bbb1d09cfd74f305155578217708ffcdfa56fe
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2026-09-15 12:17:31 +0100

    [18.0][IMP] account_journal_bank_exclusivity : log rule action_id (#363)
    * [ADD] accoubt_move_secure
    
    * [18.0][IMP] account_journal_bank_exclusivity : log rule action_id
    
    ---------
    
    Co-authored-by: Abdellatif Benzbiria <odoo-base@numigi.com>

--- account_journal_bank_exclusivity/__manifest__.py
+++ account_journal_bank_exclusivity/__manifest__.py
@@ -9,7 +9,7 @@
     "summary": """
         Guarantees uniqueness and exclusivity of bank journal accounts.
     """,
-    "version": "18.0.2.0.2",
+    "version": "18.0.2.1.0",
     "website": "https://bit.ly/numigi-com",
     "author": "Numigi",
     "maintainer": "Numigi",
